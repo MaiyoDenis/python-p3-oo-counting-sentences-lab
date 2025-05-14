26826221e372ce9749635b2dfd81189591b0cb33
DenisMaiyo <denismaiyo14@gmail.com>
2025-05-14 04:45:13 +0300

    first push

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, initial_value=""):
+        self._value = ""
+        self.value = initial_value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if not isinstance(val, str):
+            print("The value must be a string.")
+        else:
+            self._value = val
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        import re
+        # Count sentences ending with ., !, or ? including multiple punctuation marks like !! or ...
+        sentences = re.findall(r'[^.!?]*[.!?]+', self._value)
+        return len(sentences)
